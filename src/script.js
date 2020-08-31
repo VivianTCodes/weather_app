1b3ba94b6f4a96043abdf35f2e5a414349d9d2f1
VivianTCodes <70532616+VivianTCodes@users.noreply.github.com>
2020-08-31 21:57:48 +0100

    added back current location feature

--- src/script.js
+++ src/script.js
@@ -31,6 +31,8 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind");
   let iconElement = document.querySelector("#icon");
   let feelLikeElement = document.querySelector("#icon");
+  let currentLocationButton = document.querySelector("#currentCity");
+  currentLocationButton.addEventListener("click", getCurrentLocation);
 
   celsiusTemperature = response.data.main.temp;
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
@@ -54,15 +56,32 @@ function search(city) {
   let apiKey = "fd1335ecaf9ed4bc37a37b29f7cf63b5";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
+}
+function searchLocation(position) {
+  let apiKey = "fd1335ecaf9ed4bc37a37b29f7cf63b5";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
 
-  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(dispalyForecast);
+  axios.get(apiUrl).then(displayTemperature);
 }
 
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
 function handleSubmit(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
   search(cityInputElement.value);
+}
+
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
 }
 
 function displayFahrenheitTemperature(event) {
